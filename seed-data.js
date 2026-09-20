cf1a1af2f0919066e27f3890822ad3ae2923e047
transparencia-mendonca <miguel.angelogarcia@gmail.com>
2026-09-20 17:52:09 -0300

    Add files via upload

---
+++ seed-data.js
@@ -0,0 +1,2 @@
+window.SEED_DATA={_bundleVersion:"adolfo-baseline-2026-09-20",exercicios:[],despesas:[],receitas:[],apiUpdates:{},autoCheck:{}};
+window.TCESP_PERMANENT_BASELINE={manifest:"baseline-manifest.json",version:"adolfo-baseline-2026-09-20"};
